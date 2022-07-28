4852493d4f3704448897de185b8fecf80a349b0e
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-07-27 18:42:13 -0700

    Switch the technique used to call faceMesh (although I preferred the previous method after the switch). Add a partyFilter that adds glasses and a rose filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import '@mediapipe/face_mesh';
 import '@tensorflow/tfjs-core';
 // Register WebGL backend.
 import '@tensorflow/tfjs-backend-webgl';
-import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+import { VERSION as FaceMeshVersion, FaceMesh, FACEMESH_FACE_OVAL, FACEMESH_LEFT_EYE, FACEMESH_LEFT_EYEBROW, FACEMESH_LEFT_IRIS, FACEMESH_LIPS, FACEMESH_RIGHT_EYE, FACEMESH_RIGHT_EYEBROW, FACEMESH_RIGHT_IRIS, FACEMESH_TESSELATION, Results, NormalizedLandmarkList, LandmarkConnectionArray } from '@mediapipe/face_mesh';
 
 let RENDER_PREDICTION_ON_FRAME_HANDLE = -1;
-let detector: faceLandmarksDetection.FaceLandmarksDetector;
 let canvasContext: CanvasRenderingContext2D | null;
+const roseGlasses = new Image();
+const flowerCrown = new Image();
 
 const startStreaming = async (video: HTMLVideoElement, stream: MediaStream): Promise<void> => {
   video.srcObject = stream;
@@ -25,55 +27,120 @@ const getVideoFeed = async (): Promise<MediaStream | undefined> => {
   }
 }
 
-const performFaceDetection = async (videoFeed: HTMLVideoElement) => {
-  if (!detector) {
-    const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-    const detectorConfig: faceLandmarksDetection.MediaPipeFaceMeshMediaPipeModelConfig = {
-      runtime: 'mediapipe', // or 'tfjs'
-      solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-      refineLandmarks: true
-    }
-    detector = await faceLandmarksDetection.createDetector(model, detectorConfig);
-  }
-  return await detector.estimateFaces(videoFeed);
-}
-
-const drawResult = async (originalVideo: HTMLVideoElement, detectionResult: faceLandmarksDetection.Face[]) => {
-  console.log('rendering result: ', detectionResult);
+const drawResult = async (detectionResults: Results) => {
+  console.log('rendering result');
 
   if (!canvasContext) {
     const canvas = document.getElementById('augmentedVideo') as HTMLCanvasElement;
-    canvas.width = originalVideo.videoWidth;
-    canvas.height = originalVideo.videoHeight;
+    canvas.width = detectionResults.image.width;
+    canvas.height = detectionResults.image.height;
     canvasContext = canvas.getContext('2d');
 
     if (!canvasContext) {
-      alert('No convas to draw on found');
+      alert('No canvas to draw on found');
       return;
     }
   }
 
-  canvasContext.drawImage(originalVideo, 0, 0, originalVideo.videoWidth, originalVideo.videoHeight);
+  canvasContext.clearRect(0, 0, detectionResults.image.width, detectionResults.image.height);
 
-  for (const face of detectionResult) {
-    const { keypoints } = face;
+  const drawBackground = document.getElementById('backgroundToggle') as HTMLInputElement;
+  if (drawBackground?.checked) {
+    canvasContext.drawImage(detectionResults.image, 0, 0, detectionResults.image.width, detectionResults.image.height);
+  }
 
-    for (const keypoint of keypoints) {
-      const {x, y} = keypoint;
-      canvasContext.beginPath();
-      canvasContext.arc(x, y, 2, 0, 2 * Math.PI);
-      canvasContext.fillStyle = 'red';
-      canvasContext.fill();
+  const drawOption = (document.getElementById('drawOption') as HTMLSelectElement).value;
+
+  if (drawOption === 'mesh') {
+    for (const landmarks of detectionResults.multiFaceLandmarks) {
+      drawConnectors(canvasContext, landmarks, FACEMESH_TESSELATION, {color: '#C0C0C070', lineWidth: 1});
+      drawConnectors(canvasContext, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+      drawConnectors(canvasContext, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+    }
+  }
+
+  if (drawOption === 'landmarks') {
+    for (const landmarks of detectionResults.multiFaceLandmarks) {
+      drawLandmarks(canvasContext, landmarks, {color: '#ff0000', radius: 1});
+    }
+  }
+
+  if (drawOption === 'funFilter') {
+    canvasContext.fillStyle = '#B76E7949';
+    canvasContext.fillRect(0, 0, detectionResults.image.width, detectionResults.image.height);
+
+    for (const landmarks of detectionResults.multiFaceLandmarks) {
+      const rightEye = getTopLeftBottomRight(landmarks, FACEMESH_LEFT_EYE, { width: detectionResults.image.width, height: detectionResults.image.height });
+      const leftEye = getTopLeftBottomRight(landmarks, FACEMESH_RIGHT_EYE,  { width: detectionResults.image.width, height: detectionResults.image.height });
+      const head = getTopLeftBottomRight(landmarks, FACEMESH_FACE_OVAL, { width: detectionResults.image.width, height: detectionResults.image.height });
+
+      const eyeWidth = Math.abs(rightEye.right - leftEye.left);
+      const eyeHeight = Math.abs(leftEye.bottom - leftEye.top);
+
+      const leftPupil = {
+        x: leftEye.left + (leftEye.right - leftEye.left) / 2,
+        y: leftEye.top + (leftEye.bottom - leftEye.top) / 2,
+      };
+      const rightPupil = {
+        x: rightEye.left + (rightEye.right - rightEye.left) / 2,
+        y: rightEye.top + (rightEye.bottom - rightEye.top) / 2,
+      };
+      const delta = {
+        x: rightPupil.x - leftPupil.x,
+        y: rightPupil.y - leftPupil.y,
+      };
+      const rotation = Math.atan2(delta.y, delta.x);
+
+      const glassesWidth = eyeWidth * 1.75;
+      const glassesHeight = (head.bottom - head.top) / 3;
+      const glassesWidthExcess = glassesWidth - eyeWidth;
+      const glassesHeightExcess = glassesHeight - eyeHeight;
+
+      canvasContext.save();
+      canvasContext.translate(leftEye.left, leftEye.top);
+      canvasContext.rotate(rotation);
+      canvasContext.drawImage(
+        roseGlasses,
+        -glassesWidthExcess / 2,
+        -glassesHeightExcess / 2,
+        glassesWidth,
+        glassesHeight);
+
+      // canvasContext.drawImage(
+      //   flowerCrown,
+      //   -glassesWidth / 4,
+      //   -glassesHeight * 1.75,
+      //   glassesWidth * 1.1,
+      //   glassesHeight * 1.5
+      // );
+
+      canvasContext.restore();
     }
   }
 }
 
-const startFaceDetection = async (video: HTMLVideoElement) => {
-  console.log('Starting Face Detection on each animation frame');
+const getTopLeftBottomRight = (landmarks: NormalizedLandmarkList, KNOWN_LANDMARK: LandmarkConnectionArray, scale: {width: number; height: number}) => {
+  const xVals = KNOWN_LANDMARK.map((index) => landmarks[index[0]].x);
+  const yVals = KNOWN_LANDMARK.map((index) => landmarks[index[0]].y);
+
+  return {
+    top: Math.min(...yVals) * scale.height,
+    left: Math.min(...xVals) * scale.width,
+    bottom: Math.max(...yVals) * scale.height,
+    right: Math.max(...xVals) * scale.width,
+  };
+}
+
+const doFaceDetection = async (detector: FaceMesh, video: HTMLVideoElement) => {
   RENDER_PREDICTION_ON_FRAME_HANDLE = requestAnimationFrame(async () => {
-    const detectionResult = await performFaceDetection(video);
-    drawResult(video, detectionResult);
-    startFaceDetection(video);
+    await detector.send({ image: video });
+    await doFaceDetection(detector, video);
   });
 }
 
@@ -83,13 +150,30 @@ const start = async () => {
   if (!videoFeed) return;
 
   console.log('Starting Video Feed');
-  const originalVideo = document.getElementById('originalVideo') as HTMLVideoElement;
-  await startStreaming(originalVideo, videoFeed);
+  const videoElement = document.getElementById('originalVideo') as HTMLVideoElement;
+  await startStreaming(videoElement, videoFeed);
 
-  startFaceDetection(originalVideo);
+  const faceMesh = new FaceMesh({
+    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@${FaceMeshVersion}/${file}`
+  });
+  faceMesh.setOptions({
+    selfieMode: false,
+    enableFaceGeometry: true,
+    maxNumFaces: 1,
+    refineLandmarks: false
+  });
+  faceMesh.onResults(async (results) => {
+    drawResult(results);
+  });
+  doFaceDetection(faceMesh, videoElement);
 }
 
 function App() {
+
+  useEffect(() => {
+    roseGlasses.src = 'rose_glasses.png';
+    flowerCrown.src = 'flowercrown.png';
+  }, [])
 
   return (
     <div>
@@ -101,6 +185,19 @@ function App() {
           RENDER_PREDICTION_ON_FRAME_HANDLE = -1
         }
       }>Stop</button>
+      <br />
+
+      <select id="drawOption" defaultValue="funFilter">
+        <option value="mesh">Mesh</option>
+        <option value="landmarks">Landmarks</option>
+        <option value="funFilter">Fun Filter 🎉</option>
+        <option value="none">none</option>
+      </select>
+      <br />
+
+      <input id="backgroundToggle" type="checkbox" defaultChecked={true} />
+      <label htmlFor="backgroundToggle">Show Background</label>
+
       <div>
         <video id='originalVideo' autoPlay />
         <canvas id='augmentedVideo' />
